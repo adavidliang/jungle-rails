7966db469ad06f40bcaea050c12c91c9068faa39
adavidliang <adavidliang@gmail.com>
2023-07-30 20:23:46 -0400

    change Admin DashBoard

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,7 @@
 Rails.configuration.stripe = {
   :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-  :secret_key      => ENV['STRIPE_SECRET_KEY']
-  :secret_name => ENV['STRIPE_SECRET_NAME']
+  :secret_key      => ENV['STRIPE_SECRET_KEY'],
+  :secret_name => ENV['STRIPE_SECRET_NAME'],
   :secret_password => ENV['STRIPE_SECRET_PASSWORD']
 }
 
