fd268c6d77285a372623a887732900af76b5cd4d
adavidliang <adavidliang@gmail.com>
2023-07-31 00:03:02 -0400

    add Admin category

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,6 @@
 Rails.configuration.stripe = {
   :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-  :secret_key      => ENV['STRIPE_SECRET_KEY'],
-  :secret_name => ENV['STRIPE_SECRET_NAME'],
-  :secret_password => ENV['STRIPE_SECRET_PASSWORD']
+  :secret_key      => ENV['STRIPE_SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
